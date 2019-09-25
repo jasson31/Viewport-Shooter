350935390a617178a6ec16e4ef3b9e35f438b1b5
jasson31 <jasson31@naver.com>
2019-09-25 12:38:08 +0900

    임시로 카메라 전환 및 카메라에 특정 오브젝트가 들어오는 경우 판정 구현

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraManager : MonoBehaviour
+{
+    public RawImage broadcastScreen;
+    public RenderTexture[] renderTextures;
+    public Camera[] cameras;
+    public int currentCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            broadcastScreen.texture = renderTextures[0];
+            currentCamera = 0;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            broadcastScreen.texture = renderTextures[1];
+            currentCamera = 1;
+        }
+    }
+}
---
+++ Assets/Scripts/CubeObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeObject : MonoBehaviour
+{
+    bool isVisibleForCamera1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //https://stackoverflow.com/questions/46364719/unity-how-do-i-check-if-an-object-is-seen-by-the-main-camera
+        isVisibleForCamera1 = GeometryUtility.TestPlanesAABB(GeometryUtility.CalculateFrustumPlanes(GameObject.Find("CameraManager").GetComponent<CameraManager>().cameras[GameObject.Find("CameraManager").GetComponent<CameraManager>().currentCamera].GetComponent<Camera>()), GetComponent<MeshRenderer>().bounds);
+        Debug.Log(isVisibleForCamera1);
+    }
+}
