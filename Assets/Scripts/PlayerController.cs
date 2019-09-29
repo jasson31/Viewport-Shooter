c19ae1b6599b0d0236de01380d6dfeb1b04ff12d
jasson31 <jasson31@naver.com>
2019-09-29 19:03:18 +0900

    플레이어 회전 및 중력 구현 중

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10, inputX, inputZ;
+    public float speed = 10, yVelocity = 0, gravity = -20, inputX, inputZ;
     CharacterController characterController = null;
     Animator animator = null;
 
@@ -18,12 +18,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         inputX = Input.GetAxis("Horizontal");
         inputZ = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(inputX * 0.5f, 0, inputZ);
         animator.SetBool("isWalking", direction.magnitude != 0);
-
         bool forward = false, backward = false, left = false, right = false;
         if (inputZ != 0 || inputX != 0)
         {
@@ -42,7 +40,19 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("isWalkingBackward", backward);
         animator.SetBool("isWalkingLeft", left);
         animator.SetBool("isWalkingRight", right);
-
         characterController.Move(transform.TransformDirection(direction) * speed * Time.deltaTime);
+        yVelocity += gravity * Time.deltaTime;
+        direction.y = yVelocity;
+        characterController.Move(direction * Time.deltaTime);
+
+
+        float mouseMoveValueX = Input.GetAxis("Mouse X");
+        float mouseMoveValueY = Input.GetAxis("Mouse Y");
+        rotationX += mouseMoveValueX * 100 * Time.deltaTime;
+        rotationY += mouseMoveValueY * 100 * Time.deltaTime;
+
+        rotationY = Mathf.Clamp(rotationY, -20, 40);
+        transform.eulerAngles = new Vector3(-rotationY, rotationX, 0);
     }
+    float rotationX = 0, rotationY = 0;
 }
