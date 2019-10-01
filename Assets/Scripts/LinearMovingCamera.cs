5575f71755c8ebba54f175d5420241f59260b784
류지석 <roadmageb@gmail.com>
2019-10-01 21:53:52 +0900

    카메라 조정, 적 배치 WIP

---
+++ Assets/Scripts/LinearMovingCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearMovingCamera : MonoBehaviour
+{
+    public GameObject[] cameras;
+    public GameObject[] destination;
+    bool initiateMove = false;
+    float moveLerp = 0f;
+    public float moveTerm = 3f;
+
+    private void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Contains("Player"))
+        {
+            GetComponent<Collider>().enabled = false;
+            foreach (SpawnInfo spwn in GetComponentsInChildren<SpawnInfo>())
+            {
+                initiateMove = true;
+            }
+        }
+    }
+}
