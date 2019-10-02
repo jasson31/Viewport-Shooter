88b8cd17f18c2549f4292ee9b918bced67fe67d2
jasson31 <jasson31@naver.com>
2019-10-02 14:27:23 +0900

    총알 발사 구현 wip, 현재 이동하면서 총을 발사할 경우 총알 위치가 이상해지는 문제가 있음

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject firePoint, fpsGun, tpsGun;
     public float speed = 10, yVelocity = 0, gravity = -20;
     CharacterController characterController = null;
-    Animator animator = null;
+    Animator playerAnimator, tpsGunAnimator, fpsGunAnimator;
     Camera mainCamera = null;
     float rotationX = 0, rotationY = 0, sensitivity = 100, keyInputX, keyInputZ;
     
@@ -14,7 +15,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
+        playerAnimator = GetComponent<Animator>();
+        fpsGunAnimator = fpsGun.GetComponent<Animator>();
+        tpsGunAnimator = tpsGun.GetComponent<Animator>();
         mainCamera = Camera.main;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -23,10 +26,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Player move
         keyInputX = Input.GetAxis("Horizontal");
         keyInputZ = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(keyInputX * 0.5f, 0, keyInputZ);
-        animator.SetBool("isWalking", direction.magnitude != 0);
+        playerAnimator.SetBool("isWalking", direction.magnitude != 0);
         bool forward = false, backward = false, left = false, right = false;
         if (keyInputZ != 0 || keyInputX != 0)
         {
@@ -41,24 +45,31 @@ public class PlayerController : MonoBehaviour
                 else left = true;
             }
         }
-        animator.SetBool("isWalkingForward", forward);
-        animator.SetBool("isWalkingBackward", backward);
-        animator.SetBool("isWalkingLeft", left);
-        animator.SetBool("isWalkingRight", right);
+        playerAnimator.SetBool("isWalkingForward", forward);
+        playerAnimator.SetBool("isWalkingBackward", backward);
+        playerAnimator.SetBool("isWalkingLeft", left);
+        playerAnimator.SetBool("isWalkingRight", right);
         characterController.Move(transform.TransformDirection(direction) * speed * Time.deltaTime);
         yVelocity += gravity * Time.deltaTime;
         direction.y = yVelocity;
         characterController.Move(direction * Time.deltaTime);
 
-
+        //Camera rotate
         float mouseMoveValueX = Input.GetAxis("Mouse X");
         float mouseMoveValueY = Input.GetAxis("Mouse Y");
         rotationX += mouseMoveValueX * Time.deltaTime * sensitivity;
         rotationY += mouseMoveValueY * Time.deltaTime * sensitivity;
-
         rotationY = Mathf.Clamp(rotationY, -20, 30);
         transform.eulerAngles = new Vector3(0, rotationX, 0);
         mainCamera.transform.eulerAngles = new Vector3(-rotationY, mainCamera.transform.eulerAngles.y, 0);
-        animator.SetFloat("LookAngle", (rotationY + 20) / 50);
+        playerAnimator.SetFloat("LookAngle", (rotationY + 20) / 50);
+
+        //Shoot
+        if(Input.GetMouseButtonDown(0))
+        {
+            BulletFactory.inst.MakeBullet(firePoint.transform.position, firePoint.transform.rotation, firePoint.transform.up);
+            fpsGunAnimator.SetTrigger("Fire");
+            tpsGunAnimator.SetTrigger("Fire");
+        }
     }
 }
