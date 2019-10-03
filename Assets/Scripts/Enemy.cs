bba9e36c98d2f1d692f4d73a72952d3ca34e18dd
jasson31 <jasson31@naver.com>
2019-10-03 15:42:30 +0900

    적과 일정거리 안에 플레이어가 있을 경우 적이 플레이어를 향해 다가옴

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    Transform target;
+
     public void Die()
     {
         Debug.Log("으악");
@@ -21,7 +23,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
