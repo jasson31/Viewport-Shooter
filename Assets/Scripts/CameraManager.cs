003ca98e1323f4361004eb791fb31c8e417276bb
jasson31 <jasson31@naver.com>
2019-10-07 18:13:34 +0900

    일부 수치 조정함. 미니맵 구현 wip 현재 미니맵에 오브젝트가 어디에 있다 정도만 보임

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -16,19 +16,37 @@ public class CameraManager : SingletonBehaviour<CameraManager>
     void Start()
     {
         renderTextures = new RenderTexture[6];
-        for (int i = 0; i < 6; i++) renderTextures[i] = new RenderTexture(renderTexture);
-        for(int i = 0; i < 6; i++) cameraObjects[i].camera.targetTexture = renderTextures[i];
+        for (int i = 0; i < 6; i++)
+        {
+            renderTextures[i] = new RenderTexture(renderTexture);
+            cameraObjects[i].camera.targetTexture = renderTextures[i];
+            //cameraObjects[i].enabled = false;
+        }
+        ChangeCamera(0);
+    }
+
+    void ChangeCamera(int i)
+    {
+        cameraObjects[currentCamera].enabled = false;
+        currentCamera = i;
+        broadcastScreen.texture = cameraObjects[currentCamera].camera.targetTexture;
+        cameraObjects[currentCamera].enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) currentCamera = 0;
-        else if(Input.GetKeyDown(KeyCode.Alpha2)) currentCamera = 1;
-        else if(Input.GetKeyDown(KeyCode.Alpha3)) currentCamera = 2;
-        else if(Input.GetKeyDown(KeyCode.Alpha4)) currentCamera = 3;
-        else if(Input.GetKeyDown(KeyCode.Alpha5)) currentCamera = 4;
-        else if(Input.GetKeyDown(KeyCode.Alpha6)) currentCamera = 5;
-        broadcastScreen.texture = cameraObjects[currentCamera].camera.targetTexture;
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        if (scrollInput != 0) ChangeCamera((currentCamera + (scrollInput > 0 ? 1 : 5)) % 6);
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) ChangeCamera(0);
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) ChangeCamera(1);
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) ChangeCamera(2);
+        else if (Input.GetKeyDown(KeyCode.Alpha4)) ChangeCamera(3);
+        else if (Input.GetKeyDown(KeyCode.Alpha5)) ChangeCamera(4);
+        else if (Input.GetKeyDown(KeyCode.Alpha6)) ChangeCamera(5);
+
+        Camera current = cameraObjects[1].camera;
+        Gizmos.DrawFrustum(current.transform.position, current.fieldOfView, current.farClipPlane, current.nearClipPlane, current.aspect);
     }
 }
